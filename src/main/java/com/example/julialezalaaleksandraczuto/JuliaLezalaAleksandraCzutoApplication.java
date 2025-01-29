f3fd59525015c911b1289b5d808fe7ad4b23bb9d
julialezala <226815@stud.usz.edu.pl>
2025-01-29 14:23:39 +0100

    nowy projekt

---
+++ src/main/java/com/example/julialezalaaleksandraczuto/JuliaLezalaAleksandraCzutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.julialezalaaleksandraczuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuliaLezalaAleksandraCzutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuliaLezalaAleksandraCzutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/julialezalaaleksandraczuto/JuliaLezalaAleksandraCzutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.julialezalaaleksandraczuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuliaLezalaAleksandraCzutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
